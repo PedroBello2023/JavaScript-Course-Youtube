e43b9c1d995128012aef570b4f8b5956afba0130
Pedro Henrique Martins Belo <pedrohenriqueufpa2018@gmail.com>
2023-10-13 22:22:52 -0300

    JavaScript

---
+++ JS/Aula01.js
@@ -0,0 +1,5 @@
+//comandos de saída
+
+document.write('Olá, Mundo!')
+console.log('Outro Olá, Mundo!')
+alert('Olá, Mundo!')
---
+++ JS/Aula02.js
@@ -0,0 +1,26 @@
+/* Variáveis e Constantes */
+
+let mensagem = 'minha mensagem'
+let mensagem2 = 'Outra mensagem'
+let meu_peso = 72.2
+let minha_altura = 1.68
+let minha_idade = 29
+let eh_doador = false //valor booleano
+let teste = null 
+
+/* criar constantes e atribuir valor*/
+const PI = 3.1415
+const TAXA = 0.5
+
+teste = 'Algum conteúdo'
+
+/* escrever o conteúdo de variáveis e constantes */
+document.write('<p> Mensagem ' + mensagem + '</p>')
+document.write(`Mensagem2: ${mensagem2}`)
+document.write('<p> Peso ' + meu_peso + 'Kg </p>')
+document.write(`Altura: ${minha_altura}`)
+document.write('<p> Idade: ' + minha_idade + 'anos</p>')
+document.write(`Doador? ${eh_doador}`)
+document.write('<p>Teste: ' + teste + '</p>')
+document.write('<p>PI: ' + PI + '</p>')
+document.write('<p>' + TAXA + '</p>')
